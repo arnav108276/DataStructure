6aa65a6be1739710d89b079371617ec7258a6680
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-08-10 14:39:03 +0530

    Update Deletion.c

--- Binary Tree/Deletion.c
+++ Binary Tree/Deletion.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 typedef struct Node {
     int data;
     struct Node* left;
     struct Node* right;
 } Node;
-
 Node* createNode(int data) {
     Node* newNode = (Node*) malloc(sizeof(Node));
     newNode->data = data;
